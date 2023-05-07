530c114876f31849a179c6640a89b900bd200ba2
Yash Surange <88226034+ysurs@users.noreply.github.com>
2023-05-07 12:24:34 +0530

    imp checkpoint for mha for mulitiple images

--- source/mha.py
+++ source/mha.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 class muliheaded_attention(nn.Module):
@@ -35,9 +36,15 @@ class muliheaded_attention(nn.Module):
         1. patch_sequences is of shape (batch_size (no of images in a batch), no_of_patches per image, embedding_size of each patch)
         '''
         
+        '''
+        The following list contains the concatenated filtered attention for all images in the batch.
+        '''
+        after_mha=[]
+        
         for patch_sequence in patch_sequences:
             
             print(patch_sequence.shape)
+            filtered_attention_matrices=[]
             
             for h in range(self.no_of_heads):
                 
@@ -49,7 +56,35 @@ class muliheaded_attention(nn.Module):
                 per_head_v=self.v_mapping[h]
                 
                 
+                '''
+                Each head will attend to a subsequence of the total embedding.
+                '''
                 sub_sequence_per_head=patch_sequence[:,h*(self.dimensions_per_head):(h+1)*(self.dimensions_per_head)]
-                print(sub_sequence_per_head.shape)
-                print(sub_sequence_per_head)
                 
+                
+                q=per_head_q(sub_sequence_per_head)
+                k=per_head_k(sub_sequence_per_head)
+                v=per_head_v(sub_sequence_per_head)
+                
+                
+                '''
+                1. The attention values obtained below tell us how important is one patch to all the other patches including itself. We do this for all patches.
+                2. This matrix is a square matrix of the attention values.
+                '''
+                attention_per_head=self.softmax((q@k.T)/self.dimensions_per_head**0.5)
+                
+                
+                '''
+                We use attention matrix to find filtered attention values. A very good intuitive example is found here: https://youtu.be/mMa2PmYJlCo?t=775
+                '''
+                filtered_attention=attention_per_head@v
+                
+                
+                '''
+                We append the filtered attention obtained from each head to a list. After collecting these filtered_attention matrices, we will need to concatenate them horizontally.
+                '''
+                filtered_attention_matrices.append(filtered_attention)
+                print(filtered_attention)
+            
+            after_mha.append(torch.hstack(filtered_attention_matrices))
+            print(after_mha)
